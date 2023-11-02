bdd2741df29dbf6ba505114899a5b4e589cba50a
= <=>
2023-11-02 12:38:39 +0000

    first commit

---
+++ arrays.js
@@ -0,0 +1,23 @@
+//code an array of 10 elements of type string (for example 10 students)
+
+let student= ["john", "Layland", "Ali", "Liban", "Fatima", "Noora", "Hidaya", "Bilal"]
+//write a loop that iterates through each elements and prints each element
+
+for(let i=0;i<student.length;i++) {
+    console.log(students[i])
+}
+//name few functions that come with arrays, that allows you to interate through the elements
+
+//students.forEach()
+//students.map()
+//students.filter()
+//students.reduce()
+
+
+//implements forEach function that prints all elements of the array below
+
+//students.forEach(
+    //function(value,index){
+        //console.log("the elements is "+ value +"and its index is"+ index)
+//}
+//)
